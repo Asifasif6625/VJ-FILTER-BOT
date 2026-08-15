b24f19e7d6a0ae3a114ccc3d83465eb3e635c6a1
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2026-08-15 15:28:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -84,7 +84,7 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://data5mrmv:data5mrmv@cluster5.t5ollhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster5") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
